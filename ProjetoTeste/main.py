d7e8b9ae0f2a8299a2ca64716ed86126930fed1b
Joao Vitor Prado <joaovitorjprado@gmail.com>
2026-01-24 13:13:31 -0300

    Commit teste 2

--- ProjetoTeste/main.py
+++ ProjetoTeste/main.py
@@ -1,7 +1,40 @@
 import requests
+import customtkinter as ctk
 
 
-coin1 = None
-coin2 = None
+
+
+
+coin1 = str(input ("Digite a moeda que deseja a conversão: "))
+
+coin2 = str(input("Digite a moeda para ser usado de referencia na conversão: "))
 
 url_api = f'https://economia.awesomeapi.com.br/json/last/{coin1}-{coin2}'
+
+apiGet = requests.get(url_api).json()
+
+print(apiGet)
+
+apiKey = f"{coin1}-{coin2}"
+
+if apiKey in apiGet:
+    dados_moeda = apiGet[apiKey]
+
+    coin1Value = float(dados_moeda['bid'])  # Preço de compra
+    coin2Value = float(dados_moeda['ask'])  # Preço de venda
+    variacao = float(dados_moeda['varBid'])  # Variação
+    maximo = float(dados_moeda['high'])     # Máximo do dia
+    minimo = float(dados_moeda['low'])      # Mínimo do dia
+
+
+
+
+    # interface initialization
+app = ctk.CTk()
+app.geometry("400x600")
+app.title("Conversos de moedas")
+
+coinSelect = ctk.CTkEntry(app,width= 500,placeholder_text="Selecione a moeda")
+coinSelect.pack(pady=10, padx=10)
+
+app.mainloop()
