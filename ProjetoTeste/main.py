9c2d4ebb130bdcfae19c377065645a4dd4b8fefe
Joao Vitor Prado <joaovitorjprado@gmail.com>
2026-02-04 08:36:48 -0300

    Test Version

--- ProjetoTeste/main.py
+++ ProjetoTeste/main.py
@@ -1,40 +1,56 @@
 import requests
 import customtkinter as ctk
+from tkinter import messagebox
+
+def currency_converter_button():
+    coin1 = currency1.get()
+
+    coin2 = currency2.get()
 
 
+    url_api = f'https://economia.awesomeapi.com.br/json/last/{coin1}-{coin2}'
 
+    apiGet = requests.get(url_api).json()
 
+    print(apiGet)
 
-coin1 = str(input ("Digite a moeda que deseja a conversão: "))
+    apiKey = f"{coin1}-{coin2}"
 
-coin2 = str(input("Digite a moeda para ser usado de referencia na conversão: "))
+    if apiKey in apiGet:
+        dados_moeda = apiGet[apiKey]
 
-url_api = f'https://economia.awesomeapi.com.br/json/last/{coin1}-{coin2}'
-
-apiGet = requests.get(url_api).json()
-
-print(apiGet)
-
-apiKey = f"{coin1}-{coin2}"
-
-if apiKey in apiGet:
-    dados_moeda = apiGet[apiKey]
-
-    coin1Value = float(dados_moeda['bid'])  # Preço de compra
-    coin2Value = float(dados_moeda['ask'])  # Preço de venda
-    variacao = float(dados_moeda['varBid'])  # Variação
-    maximo = float(dados_moeda['high'])     # Máximo do dia
-    minimo = float(dados_moeda['low'])      # Mínimo do dia
-
+        coin1Value = float(dados_moeda['bid'])  # Buy price
+        coin2Value = float(dados_moeda['ask'])  # Sell price
+        variacao = float(dados_moeda['varBid'])  # Variation
+        maximo = float(dados_moeda['high'])     # Max of day
+        minimo = float(dados_moeda['low'])      # Min of day
 
 
 
     # interface initialization
 app = ctk.CTk()
-app.geometry("400x600")
-app.title("Conversos de moedas")
+app.geometry("400x400")
+app.title("Currency Converter")
 
-coinSelect = ctk.CTkEntry(app,width= 500,placeholder_text="Selecione a moeda")
-coinSelect.pack(pady=10, padx=10)
+ctk.CTkLabel(app, text="Select Currency to convert").pack(pady=10)
+
+currency1 = ctk.CTkComboBox(
+    app,
+    values=["USD", "BRL", "EUR", "JPY", "GBP","CAD"]
+)
+currency1.set("USD") 
+currency1.pack(pady=10)
+
+currency2 = ctk.CTkComboBox(
+    app,
+    values=["USD", "BRL", "EUR", "JPY", "GBP","CAD"]
+)
+currency2.set("USD") 
+currency2.pack(pady=10)
+
+button = ctk.CTkButton(app, text="Result", command=currency_converter_button)
+button.pack(pady=10)
+
+
 
 app.mainloop()
